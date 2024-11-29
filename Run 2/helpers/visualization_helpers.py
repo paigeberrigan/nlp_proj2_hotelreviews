bea2f873434a679a96a7d9ce12653dae9cfcca13
Paige Berrigan <92471520+paigeberrigan@users.noreply.github.com>
2024-11-29 14:11:29 -0500

    Add files via upload

--- Run 2/helpers/visualization_helpers.py
+++ Run 2/helpers/visualization_helpers.py
@@ -7,49 +7,54 @@ import os
 
 # custom palatte matching the tripadvisor colors 
 custom_palette = [
-    '#32d99c',  # Bright turquoise green
-    '#020c09',  # Very dark green
     '#ffffff',  # Pure white
+    '#858585',   # Medium gray
+    '#32d99c',  # Bright turquoise green
     '#66aa8d',  # Soft sage green
-    '#858585'   # Medium gray
+    '#020c09',  # Very dark green
 ]
 
 def plot_class_distribution(ratings_series, title="Class Distribution", save_path=None):
     class_counts = ratings_series.value_counts(sort=False).sort_index()
     plt.figure(figsize=(8, 5))
-    palette = custom_palette[:len(class_counts)] # use custom pallate
+    palette = custom_palette[:len(class_counts)] 
 
-    
     sns.barplot(
-        x=class_counts.index, 
-        y=class_counts.values, 
-        palette=palette,
-        edgecolor='black'
+        x=class_counts.index,
+        y=class_counts.values,
+        edgecolor='black',
+        palette=custom_palette,
     )
-    
-    plt.title(title, fontsize=16, fontweight='bold',palette=custom_palette)
+
+    plt.title(title, fontsize=16, fontweight='bold')  
     plt.xlabel("Rating", fontsize=14)
     plt.ylabel("Number of Reviews", fontsize=14)
     plt.xticks(ticks=range(len(class_counts)), labels=class_counts.index, fontsize=12)
     plt.yticks(fontsize=12)
-    
+
     if save_path:
-        # Ensure the directory exists before saving
         directory = os.path.dirname(save_path)
         if not os.path.exists(directory):
             os.makedirs(directory)
             print(f"Created directory: {directory}")
         plt.savefig(save_path, bbox_inches='tight')
         print(f"Figure saved to {save_path}")
-    
+
     plt.tight_layout()
     plt.show()
+
 
 def plot_confusion_matrix(y_true, y_pred, classes, title="Confusion Matrix", save_path=None):
     cm = confusion_matrix(y_true, y_pred, labels=classes)
     plt.figure(figsize=(8,6))
     
-    heatmap_palette = custom_palette[:5]  # palette for the heatmap - 5 colors only
+    heatmap_palette = [
+    '#32b99c',
+    '#28947c',
+    '#195c4e',
+    '#144a3e',
+    '#144a3e',
+    ]
     
     sns.heatmap(
         cm, 
